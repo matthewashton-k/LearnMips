3c7f21279ccc115d9cb0e542c2ed5b15dde688cb
CryoBlind <cwdean8@gmail.com>
2024-04-10 04:50:14 -0600

    added model class files and some test stuff to mainwindow.  can now run the code in the codeEdit by clicking run code button

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,34 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "Interpreter.h"
+#include <iostream>
+using std::cout;
+using std::endl;
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    connect(ui->runButton, &QPushButton::clicked, this, &MainWindow::runButtonClicked);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::runButtonClicked(){
+    //TEMPORARY TEST!
+    std::string instructions = ui->codeEdit->toPlainText().toStdString();
+
+    Interpreter interpreter(instructions);
+    try {
+        ui->console->setText(QString::fromStdString(interpreter.run()));
+    } catch (std::string err) {
+        cout << "error";
+        cout << err << endl;
+    }
+    //END TEMPORARY TEST!
 }
