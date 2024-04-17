57daefee07ea0433bd91973c49da2e94a594a254
Matthew Ashton Knochel <ffeufhd5j@relay.firefox.com>
2024-04-17 14:53:07 -0600

    "before code" gets executed when run is clicked

--- challenge.cpp
+++ challenge.cpp
@@ -24,7 +24,7 @@ std::tuple<std::string, bool> Challenge::executeCode(std::string middleCode, boo
         else outputString = "Invalid Solution";
     }
     else{
-        Interpreter interpreter(middleCode);
+        Interpreter interpreter(instructions);
         isValidSolution = true;
 
         try {
